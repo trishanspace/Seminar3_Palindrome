2e8df6216e92f8db3e1a63045d91e99810adea1d
Nataliia <nataliyatrishechkina@mail.ru>
2022-12-02 13:37:11 +0300

    task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -5,12 +5,10 @@
 23432 -> да*/
 
 int num = InputInt();
-int num.Length = num;
-int[] GetPal = PalNum(num);
 
 int InputInt()
 {
-    Console.WriteLine("Введите пятизначное число");
+    Console.WriteLine("Введите число");
     bool number = int.TryParse(Console.ReadLine(), out int num);
 
     if (number)
@@ -23,26 +21,33 @@ int InputInt()
         return -1;
     }
 }
-int num.Length (int num);
+// //определение длины числа
+// int LengthNumber(int num)
+// {
+//     if (num == 0)
+//         return 1;
 
-for (i = 0; num > 0; i++; num /= 10)
+//     int result = 0;
+
+//     while (num > 0)
+//     {
+//         num = num / 10;
+//         result++;
+//     }
+
+//     return result;
+// }
+//создание массива
+//переворот массива
+
+//int ArrayReverse = num;
 {
-    return num.Length;
+    int[] array = new int[num];
+    array.Reverse();
 }
+//сравниваем
 
-int[] PalNum (int num)
-{
-    int[] array = new int[num]; // обозначение массива
-
-    if (num.Length)
-    {
-        void Reverse(Array num, int 0, int num.Length);
-    }
-    return PalNum;
-}
-
-
-if (num == GetPal)
+if (num == )
 {
     Console.WriteLine("Это число является палиндромом");
 }
@@ -50,5 +55,61 @@ else
 {
     Console.WriteLine("Это число не является палиндромом");
 }
+
+
+
+
+//int GetPalindrome()
+
+
+
+
+
+// получаем длину числа
+//int GetLengthNum;
+// {
+//     int GetLengthNum(int num)
+//     {
+//     return (num == 0) ? 1 : (int)Math.Log10(Math.Abs(num)) + 1;
+//     }    
+// }
+
+
+
+
+
+//получение массива
+//int[] GetNum = new int[];//{количество чисел};  
+
+
+
+
+// int num.Length (int num);
+
+// for (i = 0; num > 0; i++; num /= 10)
+// {
+//     return num.Length;
+// }
+
+// int[] PalNum (int num)
+// {
+//     int[] array = new int[num]; // обозначение массива
+
+//     if (num.Length)
+//     {
+//         void Reverse(Array num, int 0, int num.Length);
+//     }
+//     return PalNum;
+// }
+
+
+// if (num == GetPal)
+// {
+//     Console.WriteLine("Это число является палиндромом");
+// }
+// else
+// {
+//     Console.WriteLine("Это число не является палиндромом");
+// }
 
 
