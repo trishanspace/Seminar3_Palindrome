df0dddf36edcad6cfc67e281cfc493094da552e0
Nataliia <nataliyatrishechkina@mail.ru>
2022-12-03 13:39:47 +0300

    task1_edit

--- task1/Program.cs
+++ task1/Program.cs
@@ -5,6 +5,8 @@
 23432 -> да*/
 
 int num = InputInt();
+int len = LengthNumber();
+
 
 int InputInt()
 {
@@ -21,95 +23,29 @@ int InputInt()
         return -1;
     }
 }
-// //определение длины числа
-// int LengthNumber(int num)
-// {
-//     if (num == 0)
-//         return 1;
 
-//     int result = 0;
-
-//     while (num > 0)
-//     {
-//         num = num / 10;
-//         result++;
-//     }
-
-//     return result;
-// }
-//создание массива
-//переворот массива
-
-//int ArrayReverse = num;
 {
-    int[] array = new int[num];
-    array.Reverse();
-}
-//сравниваем
-
-if (num == )
-{
-    Console.WriteLine("Это число является палиндромом");
-}
-else
-{
-    Console.WriteLine("Это число не является палиндромом");
+    int LengthNumber = (num == 0) ? 1 : (int)Math.Log10(Math.Abs(num)) + 1;
 }
 
+{
+    int n = num;
+    int reverse = 0;
+    while (len)
+    {
+        int i = n % 10;
+        reverse = reverse * 10 + i;
+        n = n / 10;
+    }
+}
 
-
-
-//int GetPalindrome()
-
-
-
-
-
-// получаем длину числа
-//int GetLengthNum;
-// {
-//     int GetLengthNum(int num)
-//     {
-//     return (num == 0) ? 1 : (int)Math.Log10(Math.Abs(num)) + 1;
-//     }    
-// }
-
-
-
-
-
-//получение массива
-//int[] GetNum = new int[];//{количество чисел};  
-
-
-
-
-// int num.Length (int num);
-
-// for (i = 0; num > 0; i++; num /= 10)
-// {
-//     return num.Length;
-// }
-
-// int[] PalNum (int num)
-// {
-//     int[] array = new int[num]; // обозначение массива
-
-//     if (num.Length)
-//     {
-//         void Reverse(Array num, int 0, int num.Length);
-//     }
-//     return PalNum;
-// }
-
-
-// if (num == GetPal)
-// {
-//     Console.WriteLine("Это число является палиндромом");
-// }
-// else
-// {
-//     Console.WriteLine("Это число не является палиндромом");
-// }
-
-
+{
+    if (reverse == num)
+    {
+        Console.WriteLine("да");
+    }
+    else
+    {
+        Console.WriteLine("нет");
+    }
+}
